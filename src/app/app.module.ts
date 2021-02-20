741ff53adf42184d427c50d7c318ab411b15e6bd
darkosika <osmnkpln03ozgurluk@gmail.com>
2021-02-21 01:02:43 +0200

    Create Subreddits and posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,6 +16,10 @@ import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component';
 import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { ListSubredditsComponent } from './subreddit/list-subreddits/list-subreddits.component';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 
 @NgModule({
@@ -26,17 +30,22 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     PostTileComponent,
     SideBarComponent,
     SubredditSideBarComponent,
-    VoteButtonComponent
+    VoteButtonComponent,
+    CreateSubredditComponent,
+    CreatePostComponent,
+    ListSubredditsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     AuthModule,
     NgxWebstorageModule.forRoot(),
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    FontAwesomeModule
+    FontAwesomeModule,
+    EditorModule
   ],
   providers: [
     {
--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PostModel } from './PostModel';
+import { CreatePostPayload } from '../post/create-post-payload';
 
 
 @Injectable({
@@ -12,5 +13,9 @@ export class PostService {
   constructor(private http: HttpClient) { }
   getAllPosts(): Observable<Array<PostModel>>  {
     return this.http.get<Array<PostModel>>('http://localhost:8080/api/posts/');
+  }
+
+  createPost(postPayload: CreatePostPayload): Observable<any> {
+    return this.http.post('http://localhost:8080/api/posts/', postPayload);
   }
 }
